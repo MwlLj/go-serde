44c9c08ffe3e445d9ae9739f274cc2a89357b896
MwlLj <731025894@qq.com>
2019-12-28 17:32:09 +0800

    新增自定义参数回调

--- strvar_serde/curly_braces.go
+++ strvar_serde/curly_braces.go
@@ -14,13 +14,21 @@ func CurlyBracesDeserde(input *string, obj interface{}) (*string, error) {
     if err != nil {
         return nil, err
     }
-    return curlyBracesDeserde(r, input)
+    return curlyBracesDeserde(r, input, nil)
+}
+
+func CurlyBracesDeserdeWithCustom(input *string, obj interface{}, customF *func(v *string) (*string, error)) (*string, error) {
+    r, err := change.Obj2MapStrStr(obj)
+    if err != nil {
+        return nil, err
+    }
+    return curlyBracesDeserde(r, input, customF)
 }
 
 /*
 ** 传入一个字符串, 字符串中的 {var} 使用 结构体 tag 定义的字段的值来替换
 */
-func curlyBracesDeserde(r *map[string]string, input *string) (*string, error) {
+func curlyBracesDeserde(r *map[string]string, input *string, customF *func(v *string) (*string, error)) (*string, error) {
     return curlyBracesVarParse(input, func(v *string)(*string, error) {
         var res string
         if v, ok := (*r)[*v]; ok {
@@ -29,7 +37,7 @@ func curlyBracesDeserde(r *map[string]string, input *string) (*string, error) {
             return nil, errors.New("not found")
         }
         return &res, nil
-    })
+    }, customF)
 }
 
 func CurlyBracesDeserdeMulti(input *string, objs ...interface{}) (*string, error) {
@@ -40,7 +48,18 @@ func CurlyBracesDeserdeMulti(input *string, objs ...interface{}) (*string, error
             return nil, err
         }
     }
-    return curlyBracesDeserde(&maps, input)
+    return curlyBracesDeserde(&maps, input, nil)
+}
+
+func CurlyBracesDeserdeMultiWithCustom(input *string, customF *func(v *string) (*string, error), objs ...interface{}) (*string, error) {
+    maps := map[string]string{}
+    for _, obj := range objs {
+        err := change.Obj2MapStrStrWithCollect(obj, &maps)
+        if err != nil {
+            return nil, err
+        }
+    }
+    return curlyBracesDeserde(&maps, input, customF)
 }
 
 type mode int8
@@ -48,9 +67,10 @@ const (
     _ mode = iota
     normal
     braces
+    brackets
 )
 
-func curlyBracesVarParse(input *string, f func(v *string) (*string, error)) (*string, error) {
+func curlyBracesVarParse(input *string, f func(v *string) (*string, error), customF *func(v *string) (*string, error)) (*string, error) {
     if input == nil {
         return nil, errors.New("input is nil")
     }
@@ -62,6 +82,8 @@ func curlyBracesVarParse(input *string, f func(v *string) (*string, error)) (*st
         case normal:
             if c == '{' {
                 m = braces
+            } else if c == '[' {
+                m = brackets
             } else {
                 result.WriteRune(c)
             }
@@ -74,6 +96,21 @@ func curlyBracesVarParse(input *string, f func(v *string) (*string, error)) (*st
                     return nil, e
                 }
                 result.WriteString(*r)
+                world.Reset()
+            } else {
+                world.WriteRune(c)
+            }
+        case brackets:
+            if c == ']' {
+                m = normal
+                w := world.String()
+                if customF != nil {
+                    r, e := (*customF)(&w)
+                    if e != nil {
+                        return nil, e
+                    }
+                    result.WriteString(*r)
+                }
                 world.Reset()
             } else {
                 world.WriteRune(c)
--- strvar_serde/curly_braces_test.go
+++ strvar_serde/curly_braces_test.go
@@ -3,6 +3,7 @@ package strvar_serde
 import (
     "testing"
     "fmt"
+    "errors"
 )
 
 type testStruct struct {
@@ -30,9 +31,48 @@ func TestCurlyBracesDeserde(t *testing.T) {
     fmt.Println(*r)
 }
 
+func TestCurlyBracesDeserdeWithCustom(t *testing.T) {
+    input := "hello, {name}, age: {age}, [uuid]"
+    customF := func(v *string) (*string, error) {
+        if *v == "uuid" {
+            uuid := "123456"
+            return &uuid, nil
+        }
+        return nil, errors.New("not match")
+    }
+    r, err := CurlyBracesDeserdeWithCustom(&input, &testStruct{
+        Name: "Jake",
+        Age: 20,
+    }, &customF)
+    if err != nil {
+        return
+    }
+    fmt.Println(*r)
+}
+
 func TestCurlyBracesDeserdeMulti(t *testing.T) {
     input := "hello, {name}, age: {age}"
     r, err := CurlyBracesDeserdeMulti(&input, &testStruct1{
+        Name: "Jake",
+    }, &testStruct2{
+        Age: 20,
+    })
+    if err != nil {
+        return
+    }
+    fmt.Println(*r)
+}
+
+func TestCurlyBracesDeserdeMultiWithCustom(t *testing.T) {
+    input := "hello, {name}, age: {age}, [uuid]"
+    customF := func(v *string) (*string, error) {
+        if *v == "uuid" {
+            uuid := "123456"
+            return &uuid, nil
+        }
+        return nil, errors.New("not match")
+    }
+    r, err := CurlyBracesDeserdeMultiWithCustom(&input, &customF, &testStruct1{
         Name: "Jake",
     }, &testStruct2{
         Age: 20,
@@ -49,7 +89,7 @@ func TestCurlyBracesVarParse(t *testing.T) {
         fmt.Println(*v)
         r := "xxx"
         return &r, nil
-    })
+    }, nil)
     if err != nil {
         return
     }
