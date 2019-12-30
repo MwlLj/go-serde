92b01942a0b9f6736b06fbcf2aa3284cd4e2a7f0
MwlLj <731025894@qq.com>
2019-12-30 10:07:18 +0800

    obj_to_map_str_str 支持 map 存储结构

--- obj2map/obj_to_map_str_str.go
+++ obj2map/obj_to_map_str_str.go
@@ -25,31 +25,8 @@ func Obj2MapStrStr(obj interface{}) (*map[string]string, error) {
     return &result, nil
 }
 
-func Obj2MapStrStrWithCollect(obj interface{}, maps *map[string]string) error {
-    if maps == nil {
-        return errors.New("param is nil")
-    }
+func obj2MapStrStrStructInner(value reflect.Value, valueType reflect.Type, maps *map[string]string) {
     result := *maps
-    var valueType reflect.Type
-    value := reflect.ValueOf(obj)
-    outType := reflect.TypeOf(obj)
-    outputKind := outType.Kind()
-    switch outputKind {
-    case reflect.Ptr:
-        valueType = outType.Elem()
-        value = value.Elem()
-    default:
-        valueType = reflect.TypeOf(obj)
-    }
-    /*
-    ** valueType: 指针指向的类型
-    */
-    switch valueType.Kind() {
-    case reflect.Struct:
-    case reflect.Interface:
-    default:
-        return errors.New("is not struct")
-    }
     fieldNum := value.NumField()
     for i := 0; i < fieldNum; i++ {
         field := value.Field(i)
@@ -96,6 +73,82 @@ func Obj2MapStrStrWithCollect(obj interface{}, maps *map[string]string) error {
             }
         }
         result[fieldName] = fieldValue
+    }
+}
+
+func obj2MapStrStrMapInner(value reflect.Value, valueType reflect.Type, maps *map[string]string) {
+    result := *maps
+    for _, key := range value.MapKeys() {
+        // field := value.Field(i)
+        // fieldType := valueType.Field(i)
+        field := value.MapIndex(key)
+        fieldName := key.String()
+        fieldKind := field.Type().Kind()
+        var fieldValue string
+        switch fieldKind {
+        case reflect.Ptr:
+            fieldPtrType := field.Type().Elem()
+            fieldPtrKind := fieldPtrType.Kind()
+            fieldElem := field.Elem()
+            switch fieldPtrKind {
+            case reflect.String:
+                fieldValue = fieldElem.String()
+            case reflect.Int:
+                i := fieldElem.Int()
+                fieldValue = strconv.FormatInt(i, 10)
+            case reflect.Bool:
+                b := fieldElem.Bool()
+                fieldValue = strconv.FormatBool(b)
+            case reflect.Float64:
+                f := fieldElem.Float()
+                fieldValue = strconv.FormatFloat(f, 'f', 10, 32)
+            }
+        default:
+            switch fieldKind {
+            case reflect.String:
+                fieldValue = field.String()
+            case reflect.Int:
+                i := field.Int()
+                fieldValue = strconv.FormatInt(i, 10)
+            case reflect.Bool:
+                b := field.Bool()
+                fieldValue = strconv.FormatBool(b)
+            case reflect.Float64:
+                f := field.Float()
+                fieldValue = strconv.FormatFloat(f, 'f', 10, 32)
+            }
+        }
+        result[fieldName] = fieldValue
+    }
+}
+
+func Obj2MapStrStrWithCollect(obj interface{}, maps *map[string]string) error {
+    if maps == nil {
+        return errors.New("param is nil")
+    }
+    var valueType reflect.Type
+    value := reflect.ValueOf(obj)
+    outType := reflect.TypeOf(obj)
+    outputKind := outType.Kind()
+    switch outputKind {
+    case reflect.Ptr:
+        valueType = outType.Elem()
+        value = value.Elem()
+    default:
+        valueType = reflect.TypeOf(obj)
+    }
+    /*
+    ** valueType: 指针指向的类型
+    */
+    switch valueType.Kind() {
+    case reflect.Struct:
+        obj2MapStrStrStructInner(value, valueType, maps)
+    case reflect.Interface:
+        obj2MapStrStrStructInner(value, valueType, maps)
+    case reflect.Map:
+        obj2MapStrStrMapInner(value, valueType, maps)
+    default:
+        return errors.New("is not parse")
     }
     return nil
 }
